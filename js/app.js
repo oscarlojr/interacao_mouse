042f6b25960b1ee0c4d3452c3775c774b305b6b1
Oscar Lima Oliveira Junior <oscarlojr@gmail.com>
2022-04-12 22:13:21 -0300

    Ajustes de posição
    Fazendo com que os píxels afastem-se do ponteiro.

--- js/app.js
+++ js/app.js
@@ -8,20 +8,18 @@ let particleArray = [];
 const mouse = {
     x:  null,
     y:  null,
-    radius: 150
+    radius: 250
 }
 
 window.addEventListener('mousemove', function(event){
     mouse.x = event.x;
     mouse.y = event.y;   
-    mouse.radius = 150;
-    //console.log(mouse.x, mouse.y);
 });
 
 ctx.fillStyle = 'white';
 ctx.font = '30px Verdana';
 ctx.fillText('A',0,30);
-const data = ctx.getImageData(0,0,100,100);
+const textCoordinates = ctx.getImageData(0,0,100,100);
 
 class Particle{
     constructor(x,y){
@@ -30,10 +28,10 @@ class Particle{
         this.size = 3;
         this.baseX = this.x;
         this.baseY = this.y;
-        this.density = (Math.random()*30)+1;
+        this.density = (Math.random()*40)+5;
     }
     draw(){
-        ctx.fillStyle = 'red';
+        ctx.fillStyle = 'white';
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
         ctx.fill();
@@ -46,24 +44,33 @@ class Particle{
         let forceDirectionY = dy / distance;
         let maxDistance = mouse.radius;
         let force = (maxDistance - distance) / maxDistance;
-        if(distance < 300){
-           this.x += forceDirectionX * 3;      
-           this.y += forceDirectionY * 3;      
+        let directionX = forceDirectionX * force * this.density;
+        let directiony = forceDirectionY * force * this.density;
+        if(distance < mouse.radius){
+           this.x -= directionX;      
+           this.y -= directiony;      
         } else{
-            this.size = 3;
+            if(this.x !== this.baseX){
+                let dx = this.x - this.baseX;
+                this.x -= dx/10;
+            }
+            if(this.y !== this.baseY){
+                let dy = this.y - this.baseY;
+                this.y -= dy/10;
+            }
         }
     }
 }
-
+console.log(textCoordinates.data);
 function init(){
     particleArray = [];
-    for(let i = 0; i <1000; i++){
-        let x = Math.random() * canvas.width;
-        let y = Math.random() * canvas.height;
-        particleArray.push(new Particle(x,y));
-    }
-    //particleArray.push(new Particle(50,50));
-    //particleArray.push(new Particle(80,50));
+   for(let y = 0, y2 = textCoordinates.height; y < y2; y++ ){
+       for (let x = 0, x2 = textCoordinates.width; x < x2; x++){
+           if(textCoordinates.data[1] > 128){
+
+           }
+       }
+   }
 }
 init();
 console.log(particleArray);
